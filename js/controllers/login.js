04a1fe844982633bcf6ddc0ed2c27ad9ccdf249d
iver56 <iver56@hotmail.com>
2014-09-22 12:28:48 +0200

    Add table with bound data. Add style.css. Add title, meta tag etc.

--- js/controllers/login.js
+++ js/controllers/login.js
@@ -1,12 +1,13 @@
 angelikaControllers.controller('LoginCtrl', function($scope, $http, cfg) {
   console.log('LoginCtrl is running!');
-  $scope.apiUrl = cfg.apiUrl;
+  $scope.alarms = [];
 
   //Hello world (API)
   $http.get(cfg.apiUrl + "/alarms/")
     .success(function(data) {
       console.log("Yay, got data from the API!");
       console.log(data);
+      $scope.alarms = data.results;
     })
     .error(function(data, status, headers, config) {
       console.log(data, status, headers, config);
