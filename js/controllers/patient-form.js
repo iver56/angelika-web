4c9da67936a0c16596717bd72769f8bbab504d93
iver56 <iver56@hotmail.com>
2014-10-23 12:24:01 +0200

    Implement add and remove next of kin @ patient form

--- js/controllers/patient-form.js
+++ js/controllers/patient-form.js
@@ -8,11 +8,12 @@ angelikaControllers.controller('PatientFormCtrl', function($scope, $http, cfg) {
     $scope.posting = true;
     $http.patch(cfg.apiUrl + "/patients/" + $scope.patientId + "/", $scope.patient)
       .success(function(patient) {
-        $scope.posting = false;
+        $scope.patient = patient;
         var fullName = patient.user.first_name + " " + patient.user.last_name;
-        var $tab = $(".lm_tab[data-patient-id='" + $scope.patientId + "']");
+        var $tab = $(".lm_tab[data-patient-id='" + $scope.patient.id + "']");
         $tab.attr('title', fullName);
         $tab.find('span.lm_title').text(fullName);
+        $scope.posting = false;
       })
       .error(function(data) {
         $scope.posting = false;
@@ -20,4 +21,18 @@ angelikaControllers.controller('PatientFormCtrl', function($scope, $http, cfg) {
       });
   };
 
+  $scope.addNextOfKin = function() {
+    $scope.patient.next_of_kin.push({
+      id: null,
+      full_name: '',
+      address: '',
+      phone_number: '',
+      priority: $scope.patient.next_of_kin[$scope.patient.next_of_kin.length - 1].priority + 1,
+      relation: ''
+    });
+  };
+
+  $scope.removeNextOfKin = function(index) {
+    $scope.patient.next_of_kin.splice(index, 1);
+  };
 });
