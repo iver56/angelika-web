5d2f0b85677bcb5382c5ba43aacd5602e581ef0c
larstva <larstorev@gmail.com>
2014-10-25 00:26:16 +0200

    - Moved function for converting milliseconds to UTC date to a service - Changed <br/> to <br> - Fixed width issues for lists

--- js/controllers/patient-lists.js
+++ js/controllers/patient-lists.js
@@ -1,7 +1,6 @@
-angelikaControllers.controller('PatientListsCtrl', function($scope) {
+angelikaControllers.controller('PatientListsCtrl', function($scope, DateTimeHelper) {
   $scope.getPatientO2Data().then(function(o2DataAPI) {
     $scope.o2Data = o2DataAPI.results;
-    console.log($scope.o2Data);
   });
 
   $scope.getPatientHeartRateData().then(function(heartRateDataAPI) {
@@ -16,14 +15,5 @@ angelikaControllers.controller('PatientListsCtrl', function($scope) {
     $scope.activityData = activityDataAPI.results;
   });
 
-  var toUTCDate = function(date){
-    var _utc = new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(),  date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds());
-    return _utc;
-  };
-
-  var millisToUTCDate = function(millis){
-    return toUTCDate(new Date(millis));
-  };
-
-  $scope.millisToUTCDate = millisToUTCDate;
+  $scope.millisToUTCDate = DateTimeHelper.millisToUTCDate;
 });
