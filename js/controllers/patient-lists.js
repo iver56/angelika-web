1fd2f6edce2605676e588e83ea6812437889d8a4
larstva <larstorev@gmail.com>
2014-10-24 23:04:17 +0200

    - Created list view - Created promise in patient.js for patient health data for use in both graph and list controllers

--- js/controllers/patient-lists.js
+++ js/controllers/patient-lists.js
@@ -1,3 +1,29 @@
 angelikaControllers.controller('PatientListsCtrl', function($scope) {
+  $scope.getPatientO2Data().then(function(o2DataAPI) {
+    $scope.o2Data = o2DataAPI.results;
+    console.log($scope.o2Data);
+  });
 
+  $scope.getPatientHeartRateData().then(function(heartRateDataAPI) {
+    $scope.heartRateData = heartRateDataAPI.results;
+  });
+
+  $scope.getPatientTemperatureData().then(function(temperatureDataAPI) {
+    $scope.temperatureData = temperatureDataAPI.results;
+  });
+
+  $scope.getPatientActivityData().then(function(activityDataAPI) {
+    $scope.activityData = activityDataAPI.results;
+  });
+
+  var toUTCDate = function(date){
+    var _utc = new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(),  date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds());
+    return _utc;
+  };
+
+  var millisToUTCDate = function(millis){
+    return toUTCDate(new Date(millis));
+  };
+
+  $scope.millisToUTCDate = millisToUTCDate;
 });
--- js/controllers/patient.js
+++ js/controllers/patient.js
@@ -19,6 +19,27 @@ angelikaControllers.controller('PatientCtrl', function($scope, $http, cfg, $q) {
   $scope.getPatient = function() {
     return asyncPatient.promise;
   };
+
+  var asyncPatientO2Data = $q.defer();
+  $scope.getPatientO2Data = function() {
+    return asyncPatientO2Data.promise;
+  };
+
+  var asyncPatientHeartRateData = $q.defer();
+  $scope.getPatientHeartRateData = function() {
+    return asyncPatientHeartRateData.promise;
+  };
+
+  var asyncPatientTemperatureData = $q.defer();
+  $scope.getPatientTemperatureData = function() {
+    return asyncPatientTemperatureData.promise;
+  };
+
+  var asyncPatientActivityData = $q.defer();
+  $scope.getPatientActivityData = function() {
+    return asyncPatientActivityData.promise;
+  };
+
   $scope.getPatientId().then(function(patientId) {
     $http.get(cfg.apiUrl + "/patients/" + patientId + "/")
       .success(function(data) {
@@ -26,6 +47,42 @@ angelikaControllers.controller('PatientCtrl', function($scope, $http, cfg, $q) {
       })
       .error(function(data, status, headers, config) {
         asyncPatient.reject(data);
+        console.error(data, status, headers, config);
+      });
+
+    $http.get(cfg.apiUrl + "/measurements/?patient_id=" + patientId + "&type=O")
+      .success(function(data) {
+        asyncPatientO2Data.resolve(data);
+      })
+      .error(function(data, status, headers, config) {
+        asyncPatientO2Data.reject(data);
+        console.error(data, status, headers, config);
+      });
+
+    $http.get(cfg.apiUrl + "/measurements/?patient_id=" + patientId + "&type=P")
+      .success(function(data) {
+        asyncPatientHeartRateData.resolve(data);
+      })
+      .error(function(data, status, headers, config) {
+        asyncPatientHeartRateData.reject(data);
+        console.error(data, status, headers, config);
+      });
+
+    $http.get(cfg.apiUrl + "/measurements/?patient_id=" + patientId + "&type=T")
+      .success(function(data) {
+        asyncPatientTemperatureData.resolve(data);
+      })
+      .error(function(data, status, headers, config) {
+        asyncPatientTemperatureData.reject(data);
+        console.error(data, status, headers, config);
+      });
+
+    $http.get(cfg.apiUrl + "/measurements/?patient_id=" + patientId + "&type=A")
+      .success(function(data) {
+        asyncPatientActivityData.resolve(data);
+      })
+      .error(function(data, status, headers, config) {
+        asyncPatientActivityData.reject(data);
         console.error(data, status, headers, config);
       });
   });
