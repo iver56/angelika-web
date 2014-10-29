f0df460457585eb14e0fc7fb4a57ae76af9dd492
MartinMekk <martin.solheim@me.com>
2014-10-29 15:07:27 +0100

    Update alarms every 5 sec

--- js/controllers/alarms.js
+++ js/controllers/alarms.js
@@ -1,14 +1,18 @@
-angelikaControllers.controller('AlarmsCtrl', function($scope, $http, cfg, AlarmHelper, LayoutUtils) {
+angelikaControllers.controller('AlarmsCtrl', function($scope, $http, $timeout, cfg, AlarmHelper, LayoutUtils) {
   $scope.alarms = [];
   $scope.measurementType = AlarmHelper.measurementType;
 
-  $http.get(cfg.apiUrl + "/alarms/")
-    .success(function(data) {
-      $scope.alarms = data.results;
-    })
-    .error(function(data, status, headers, config) {
-      console.error(data, status, headers, config);
-    });
+  (function tick() {
+    $http.get(cfg.apiUrl + "/alarms/")
+      .success(function(data) {
+        $scope.alarms = data.results;
+        $timeout(tick, 5000);
+      })
+      .error(function(data, status, headers, config) {
+        console.error(data, status, headers, config);
+        $timeout(tick, 20000);
+      });
+  })();
 
   $scope.openPatient = function(patient) {
     LayoutUtils.openPatient(patient);
