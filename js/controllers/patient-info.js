de700aa66924c8c9b391c39d0fe798db8dab2b5b
MartinMekk <martin.solheim@me.com>
2014-10-29 14:03:50 +0100

    Use alarm from api in patient info
    Get the alarm(s) for a user info page from the server.

--- js/controllers/patient-info.js
+++ js/controllers/patient-info.js
@@ -1,41 +1,18 @@
-angelikaControllers.controller('PatientInfoCtrl', function($scope, $modal) {
+angelikaControllers.controller('PatientInfoCtrl', function($scope, $http, cfg, $modal) {
 
   $scope.getPatient().then(function(patient) {
     $scope.patient = patient;
   });
 
-  //TODO: get this from the API
-  $scope.alarms = [
-    {
-      time_created: "2014-10-10T06:08:51Z",
-      is_treated: false,
-      treated_text: "",
-      value: 87,
-      type: 'O',
-      treated_text: ''
-    },
-    {
-      time_created: "2014-10-01T01:49:34Z",
-      is_treated: true,
-      type: 'P',
-      value: 110,
-      treated_text: "Hjerteflimmer."
-    },
-    {
-      time_created: "2014-09-22T22:04:07Z",
-      is_treated: true,
-      type: 'T',
-      value: 38.4,
-      treated_text: "Feber."
-    },
-    {
-      time_created: "2014-09-03T10:28:07Z",
-      is_treated: true,
-      type: 'P',
-      value: 98,
-      treated_text: "Fall i trapp."
-    }
-  ];
+  $scope.getPatientId().then(function(patientId) {
+    $http.get(cfg.apiUrl + "/alarms/?patient_id=" + patientId)
+      .success(function(data) {
+        $scope.alarms = data.results;
+      })
+      .error(function(data, status, headers, config) {
+        console.error(data, status, headers, config);
+      });
+  });
 
   $scope.open = function(size, idx) {
     var modalInstance = $modal.open({
@@ -108,7 +85,7 @@ angelikaControllers.controller('handleAlertModalInstanceCtrl', function($scope, 
     var type;
     switch (typeChar) {
       case "O":
-        type = "o2-metning";
+        type = "O2-metning";
         break;
       case "P":
         type = "puls";
