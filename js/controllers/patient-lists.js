a4082353ee648dce2aba62b17970f459ed42e317
larstva <larstorev@gmail.com>
2014-10-31 11:09:46 +0100

    Add red background and ng-click to cells with abnormal values.

--- js/controllers/patient-lists.js
+++ js/controllers/patient-lists.js
@@ -1,19 +1,25 @@
 angelikaControllers.controller('PatientListsCtrl', function($scope, DateTimeHelper) {
   $scope.getPatientO2Data().then(function(o2DataAPI) {
-    $scope.o2Data = o2DataAPI.results;
+    $scope.o2Data = o2DataAPI.measurements;
   });
 
   $scope.getPatientHeartRateData().then(function(heartRateDataAPI) {
-    $scope.heartRateData = heartRateDataAPI.results;
+    $scope.heartRateData = heartRateDataAPI.measurements;
   });
 
   $scope.getPatientTemperatureData().then(function(temperatureDataAPI) {
-    $scope.temperatureData = temperatureDataAPI.results;
+    $scope.temperatureData = temperatureDataAPI.measurements;
   });
 
   $scope.getPatientActivityData().then(function(activityDataAPI) {
-    $scope.activityData = activityDataAPI.results;
+    $scope.activityData = activityDataAPI.measurements;
   });
 
   $scope.millisToUTCDate = DateTimeHelper.millisToUTCDate;
+
+  $scope.openAlertHandling = function(measurement) {
+    if (measurement.alert) {
+      alert("test for opening alert handling");
+    }
+  }
 });
