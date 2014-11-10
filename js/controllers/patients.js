df67c884e4e1c3b263d2dbfaa118ccba88ab0b46
iver56 <iver56@hotmail.com>
2014-11-10 16:13:20 +0100

    Handle server error on patients list

--- js/controllers/patients.js
+++ js/controllers/patients.js
@@ -1,11 +1,13 @@
 angelikaControllers.controller('PatientsCtrl', function($scope, $http, cfg, AuthService) {
   $scope.patients = [];
+  $scope.serverError = false;
 
   $http.get(cfg.apiUrl + "/patients/")
     .success(function(data) {
       $scope.patients = data.results;
     })
     .error(function(data, status, headers, config) {
+      $scope.serverError = true;
       console.error(data, status, headers, config);
     });
 
